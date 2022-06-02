420e4e4d081cb32ca4e8175e35daf9584d20e659
martcpp <mordecaiemmanueletukud@gmail.com>
2022-06-02 10:49:38 -0700

    done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+/**
+ *  main - main block
+ *
+ *  Return: 9
+ */
+int main(void)
+{
+	int s;
+	s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",s);
+	return (9)
+}
