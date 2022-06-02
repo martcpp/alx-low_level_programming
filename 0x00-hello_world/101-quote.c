81dcd37068f8f7955f4dce0ae936eeb6d88c01f6
martcpp <mordecaiemmanueletukud@gmail.com>
2022-06-02 10:51:16 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,5 +11,5 @@ int main(void)
 	int s;
 	s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",s);
-	return (9)
+	return (9);
 }
