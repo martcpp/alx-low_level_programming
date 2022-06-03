3b4d92ee5673e608944cf3a803fbcd690ee54e71
martcpp <mordecaiemmanueletukud@gmail.com>
2022-06-03 14:19:49 -0700

    corected a code

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,15 +15,15 @@ int main(void)
 		last = n % 10;
 		if (last > 5)
 		{
-			printf("Last digit of %d is %d and is greater than 5\n", n, last);
+			printf("%d is postive\n", n);
 		}
 		else if (last < 6 && last != 0)
 		{
-			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+			printf("%d is negative\n", nt);
 		}
 		else
 		{
-			printf("Last digit of %d is %d and is 0\n", n, last);
+			printf("%d is zero\n", n);
 		}
 		return (0);
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	}
-	else 
+	else
 	{
 		printf("Last digit of %d is %d and is 0\n", n, last);
 	}
