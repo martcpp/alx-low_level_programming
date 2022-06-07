63708ba6d0d1f75effb60d389bbe860e7a545df6
martcpp <mordecaiemmanueletukud@gmail.com>
2022-06-07 15:32:04 -0700

    cotrection

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - finds and prints the sum of the even-valued terms
- *
+ * description - franbscci
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
 	int i = 0;
 	unsigned long int j = 1, k = 2, next = 0, sum = 0;
--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - finds and prints the first 98 Fibonacci numbers,
- *
+ * description - fibonacci
  * Return: ALways 0 (Success)
  */
-int main()
+int main(void)
 {
 	unsigned long int i = 0, j = 1, k = 2, j_1 = 0, j_2 = 0, k_1 = 0, k_2 = 0;
 
@@ -23,8 +23,8 @@ int main()
 
 	for (i = 92; i < 99; ++i)
 	{
-		printf(", %lu", k1 + (k2 / 1000000000));
-		printf("%lu", k2 % 1000000000);
+		printf(", %lu", k_1 + (k_2 / 1000000000));
+		printf("%lu", k_2 % 1000000000);
 		k_1 = k_1 +j_1;
 		j_1 = k_1 - j_1;
 		k_2 = k_2 + j_2;
