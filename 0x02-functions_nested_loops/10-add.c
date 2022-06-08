fb4e6c6658424921cf03b9717e24a0f390a03ed5
martcpp <mordecaiemmanueletukud@gmail.com>
2022-06-07 18:54:27 -0700

    Description: give two integers, returns sum  *

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,9 @@
 #include "main.h"
-/*
- * int  add - adds two integer
- *@num1: int data type
- *@num2: ibt data tyoe
- *Description: give two integers, returns sum
+/**
+ * add - adds two integers
+ * @num1: knt data tyoe
+ * @numw: int data type
+ * Description: give two integers, returns sum
  * Return: result
  */
 int add(int num1, int num2)
