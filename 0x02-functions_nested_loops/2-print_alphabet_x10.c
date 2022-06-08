760a058088b67c3eeb63c072e6e52b455152ae9b
martcpp <mordecaiemmanueletukud@gmail.com>
2022-06-07 18:40:58 -0700

    fix bug

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_alphabet_x10 - print alphabet 10 times
- *  
+ *
  *  Return: void
  */
 void print_alphabet_x10(void)
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	char letter = 'a';
 	int i = 0;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
 		letter = 'a';
 
