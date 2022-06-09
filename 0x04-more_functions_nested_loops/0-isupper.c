d545d5f33a31b1dc2b638305639f112726e7bb9b
martcpp <mordecaiemmanueletukud@gmail.com>
2022-06-09 05:34:53 -0700

    correvyion

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 /**
  * _isupper - a function that checks for uppercase characters
  * @c: a variable that stores letters
- * Return: Returns a value of 1 if the variable c contains an uppercase
+ * Return: Returns a value of 1 if the variable c contains an upperca
  */
 int _isupper(int c)
 {
--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -10,7 +10,7 @@ int _isdigit(int c)
 	{
 		return (1);
 	}
-	else 
+	else
 	{
 		return (0);
 	}
