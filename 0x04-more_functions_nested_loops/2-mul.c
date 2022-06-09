7782efb0fea5aee71ca4e7472a9be0a1680de7b8
martcpp <mordecaiemmanueletukud@gmail.com>
2022-06-09 04:28:30 -0700

    work

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,10 +3,9 @@
  * mul - function that multiplies two integers.
  * @a: int type number
  * @b: int type number
- * Return: result
+ * Return: a * b
  */
 int mul(int a, int b)
 {
-	result = a*b;
-	return(result);
+	return(a * b);;
 }
