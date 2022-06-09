e68f21f9013a176c19694a8a4c32e010f2f0552b
martcpp <mordecaiemmanueletukud@gmail.com>
2022-06-09 10:31:00 -0700

    added a fike

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,4 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-int _putchar(char c);
+int _putchar(char);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -12,4 +10,3 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-#endif
