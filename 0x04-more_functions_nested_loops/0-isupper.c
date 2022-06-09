971b7c3ac1092d7a14f29e66f7ee06d552f0346e
martcpp <mordecaiemmanueletukud@gmail.com>
2022-06-09 10:26:43 -0700

    error gix

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _isupper - a function that checks for uppercase characters
- * @c: a variable that stores letters
- * Return: Returns a value of 1 if the variable c contains an upperca
+ *@c: chars
+ * Return: a value of 1 if the variable c contains an upperca
  */
 int _isupper(int c)
 {
