a8758ebf69e9f1a7be4e6758cdf353202ccb9218
martcpp <mordecaiemmanueletukud@gmail.com>
2022-06-09 05:11:28 -0700

    ...

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * _isdigit - function that checks for a digit (0 through 9).
- *
+ * _isdigit - function that checks for a digit (0 through 9)
  * @c: int type number.
  * Return: 1 if c is a digit, 0 otherwise
  */
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_most_numbers - prints the numbers, from 0 to 9, followed by a new line
+ * print_most_numbers - prints the numbers, from 0 to 9, followed by 
  * Return: void
  */
 void print_most_numbers(void)
