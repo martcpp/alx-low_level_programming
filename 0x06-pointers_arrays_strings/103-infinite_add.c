b54cafd26d54f9573fa87a1bc2500f8317166aa9
martcpp <mordecaiemmanueletukud@gmail.com>
2022-06-16 12:20:09 -0700

    fdeg

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,99 +1,51 @@
 #include "main.h"
-
 #include <stdio.h>
-
 /**
-
  * infinite_add -   a function that adds two numbers.
-
  * @n1: first parameter.
-
  * @n2: second parameter.
-
  * @r: third parameter.
-
  * @size_r: fourth parameter
-
  * Return: 0
-
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
-
 {
-
-	/* local variable declaration */	int i = 0, j = 0, k, l = 0, f, s, d = 0;
+	int i = 0, j = 0, k, l = 0, f, s, d = 0;
 
 	while (n1[i] != '\0')
-
 		i++;
-
 	while (n2[j] != '\0')
-
 		j++;
-
 	if (i > j)
-
 		l = i;
-
 	else
-
 		l = j;
-
 	if (l + 1 > size_r)
-
 		return (0);
-
 	r[l] = '\0';
-
 	for (k = l - 1 ; k >= 0 ; k--)
-
 	{
-
 		i--;
-
 		j--;
-
 		if (i >= 0)
-
 			f = n1[i] - '0';
-
 		else
-
 			f = 0;
-
 		if (j >= 0)
-
 			s = n2[j] - '0';
-
 		else
-
 			s = 0;
-
 		r[k] = (f + s + d) % 10 + '0';
-
 		d = (f + s + d) / 10;
-
 	}
-
 	if (d == 1)
-
 	{
-
 		r[l + 1] = '\0';
-
 		if (l + 2 > size_r)
-
 			return (0);
-
 		while (l-- >= 0)
-
 			r[l + 1] = r[l];
-
 		r[0] = d + '0';
-
 	}
-
 	return (r);
-
 }
