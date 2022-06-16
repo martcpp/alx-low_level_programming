3e6ee7ee95fc9b6201c8550c7d2c499683d62d03
martcpp <mordecaiemmanueletukud@gmail.com>
2022-06-16 09:35:08 -0700

    done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,37 +1,21 @@
-
 #include "main.h"
-
 /**
-
  * cap_string - capitalizes everey word of a string
-
  * @s: string to modify
-
  *
-
  * Return: the resulting string
-
  */
 
 char *cap_string(char *s)
-
 {
+	int i, j;
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.','!', '?', '"', '(', ')', '{', '}'};
 
- int i, j;
-
- char spe[13] = {' ', '\t', '\n', ',', ';', '.',
-
-  '!', '?', '"', '(', ')', '{', '}'};
-
- for (i = 0; s[i] != '\0'; i++)
-
- {
-
-  if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
-
-   s[i] -= 32;
-
-  for (j = 0; j < 13; j++)
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+		///for (j = 0; j < 13; j++)
 
   {
 
