62b3f0278eb54d819eafbfb651bc320b5bc81a12
martcpp <mordecaiemmanueletukud@gmail.com>
2022-06-16 08:36:42 -0700

    done

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * reverse_array - reverses the content of an array
+ * @a: array
+ *  @n: number of elements of the array.
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j, temp;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(a + j);
+			*(a + j) = *(a + (j - 1));
+			*(a + (j - 1)) = temp;
+		}
+	}
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+ pointer sbd arrays
