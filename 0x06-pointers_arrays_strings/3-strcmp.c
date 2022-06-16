0878a2b327f2cfac05e1ffe9a4964a7e4bd06ff8
martcpp <mordecaiemmanueletukud@gmail.com>
2022-06-16 09:02:40 -0700

    fixed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  * _strcmp - compares two strings
- * s1: first string.
+ * @s1: first string.
+ * @s2: second string
  * Return: 0 if s1 and s2 are equals,
  * another number if not
  */
