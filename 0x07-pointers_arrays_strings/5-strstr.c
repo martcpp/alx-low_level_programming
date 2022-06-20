51ab9ac12fcf9bb2d97edf053f30bd852004e3f9
MORDECAI ETUKUDO <103220475+martcpp@users.noreply.github.com>
2022-06-20 15:44:40 +0100

    Update 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+#include <string.h>
+
+/**
+
+ * _strstr - a function that locates a substring.
+
+ *
+
+ * @haystack: This is the C string to be scanned
+
+ * @needle: second parameter
+
+ * Return: a pointer to the first occurrence of
+
+ * needle in haystack or a null pointer if
+
+ * needle is not part of haystack
+
+ */
+
+char *_strstr(char *haystack, char *needle)
+
+{
+
+	return (strstr(haystack, needle));}
