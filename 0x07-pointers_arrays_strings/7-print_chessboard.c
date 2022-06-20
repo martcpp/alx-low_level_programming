dbd76634b19cb1557f171d972d260d3244133db8
MORDECAI ETUKUDO <103220475+martcpp@users.noreply.github.com>
2022-06-20 15:43:29 +0100

    Update 7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+#include <string.h>
+
+/**
+
+ * print_chessboard - a function that prints the chessboard.
+
+ *
+
+ * @a:as parameter
+
+ */
+
+void print_chessboard(char (*a)[8])
+
+{
+
+	int b, c;
+
+	for (b = 0; b < 8; b++)
+
+	{
+
+		for (c = 0; c < 8; c++)
+
+		{
+
+			_putchar(a[b][c]);
+
+		}
+
+		_putchar('\n');
+
+	}
+
+}
