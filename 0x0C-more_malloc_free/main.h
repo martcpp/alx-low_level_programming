c29f25526ca28b772c0f41177ba85592095de5ab
martcpp <mordecaiemmanueletukud@gmail.com>
2022-06-30 15:16:13 -0700

    a

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,17 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void _error(void);
-int _isnumber(char *str);
-int _strlen(char *s);
-char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
-char *_memset(char *s, char b, unsigned int n);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-char *_memcpy(char *dest, char *src, unsigned int n);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
 
 #endif
