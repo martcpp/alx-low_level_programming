3c97efbdecba5d8f3aa5e09366fe6810d7c72b89
martcpp <mordecaiemmanueletukud@gmail.com>
2022-07-04 17:11:26 -0700

    fixing

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,9 +17,9 @@ typedef struct dog dog_t;
  */
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
